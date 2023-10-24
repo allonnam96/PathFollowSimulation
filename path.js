f722bf168ae27ca5dfb1b06bb707a88160a85211
Allon Nam <allon.nam96@gmail.com>
2023-10-24 15:24:49 -0400

    obstacle avoid

---
+++ path.js
@@ -0,0 +1,54 @@
+
+
+class Path {
+  constructor() {
+    this.originalRadius = 30;
+    this.radius = this.originalRadius; // Store the current radius
+    this.points = [];
+    this.obstacles = []; // Store obstacles
+  }
+  
+
+  // Add a point to the path
+  addPoint(x, y) {
+    let point = createVector(x, y);
+    this.points.push(point);
+  }
+
+  addObstacle(x, y, obstacleRadius) {
+    let obstacle = createVector(x, y, obstacleRadius);
+    this.obstacles.push(obstacle);
+  }
+
+  // Draw the path
+  display() {
+    strokeJoin(ROUND);
+    
+
+    // Draw thick line for radius
+    // stroke(175);
+    // strokeWeight(this.radius * 2);
+    // noFill();
+    // beginShape();
+    // for (let v of this.points) {
+    //   vertex(v.x, v.y);
+    // }
+    // endShape(CLOSE);
+    // Draw thin line for center of path
+    stroke(0);
+    strokeWeight(3.5);
+    noFill();
+    beginShape();
+    for (let v of this.points) {
+      vertex(v.x, v.y);
+    }
+    endShape(CLOSE);
+
+    for (let obstacle of this.obstacles) {
+      fill(0, 0, 0); // black color for obstacles
+      // noFill(); 
+      noStroke();
+      ellipse(obstacle.x, obstacle.y, obstacle.z * 2); // Draw obstacles as circles
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+
+// Using this variable to decide whether to draw all the stuff
+let debug = false;
+
+// A path object (series of connected points)
+let path;
+
+// Two vehicles
+let vehicles = [];
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  // Call a function to generate new Path object
+  newPath();
+
+  // We are now making random vehicles and storing them in an ArrayList
+  // for (let i = 0; i < 120; i++) {
+  //   newVehicle(random(width), random(height));
+  // }
+
+  for (let i = 0; i < path.points.length; i+=2) {
+    let point = path.points[i];
+    path.addObstacle(point.x, point.y, 30); // Assuming a radius of 30 for each obstacle
+  }
+
+}
+
+function draw() {
+  background(240);
+  // Display the path
+  path.display();
+
+  for (let v of vehicles) {
+    // Path following and separation are worked on in this function
+    v.applyBehaviors(vehicles, path);
+    // Call the generic run method (update, borders, display, etc.)
+    v.run();
+  }
+}
+
+let obstacles = [];
+
+function newPath() {
+  path = new Path();
+
+  let centerX = width / 2;
+  let centerY = height / 2 + height * 0.03;
+  let outerRadius = min(width, height) * 0.40;
+  let innerRadius = outerRadius * 0.5;
+
+  for (let i = 0; i < 10; i++) {
+    let angleOffset = -HALF_PI;
+    let angle = TWO_PI / 10 * i + angleOffset;
+    let radius;
+    if (i % 2 == 0) {
+      radius = outerRadius + 30;  // Adjusted the outer points to account for obstacle radius
+    } else {
+      radius = innerRadius;
+    }
+    let x = centerX + radius * cos(angle);
+    let y = centerY + radius * sin(angle);
+    path.addPoint(x, y);
+  }
+}
+
+
+function mousePressed() {
+  let maxspeed = random(2, 4);
+  let maxforce = 0.3;
+  vehicles.push(new Vehicle(mouseX, mouseY, maxspeed, maxforce));
+}
+
+
+function keyPressed() {
+  if (key == "d") {
+    debug = !debug;
+  }
+}
+
